7e248d4e7d44369c9f60796a6c0dfee310db4e1e
MichaelCook <michael@evoluted.net>
2016-09-08 13:27:34 +0100

    Improving bat,man

--- browser-sync-tunnel.bat
+++ browser-sync-tunnel.bat
@@ -1,7 +1,7 @@
 @echo off
 echo.
 echo Note: This also needs scotchbox to be running, as it proxies to the .dev domain
-echo Also: It uses the ../../ folder's name for the assumed project.dev url
+echo Also: It uses the ../../../ folder's name for the assumed project.dev url
 echo       I know it's a bit much, but feel free to change it if not useful to you.
 echo.
 
@@ -9,5 +9,7 @@ cd ../../../
 
 REM Get current folder name
 for %%* in (.) do set folderName=%%~nx*
+
+echo Assuming project URL: %folderName%.dev
 
 browser-sync start --proxy "%folderName%.dev" --files "build/*.css" --reload-delay 0 --tunnel --open tunnel
